1efb6cb7f052bf1c0d80c73159764c2fb240f6dd
Andrea Gilmozzi <agilmozzi@gmail.com>
2026-01-02 01:25:00 +0100

    add lsp from kickstarter

--- lua/gilmo/plugins/telescope.lua
+++ lua/gilmo/plugins/telescope.lua
@@ -1,13 +1,16 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.2.0",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope keymaps" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Telescope current word" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
+	end,
 }
